8e9b89b4e732a10ab9665afdd01ed1561caf0b4b
d4xwrld <reyzcraft98@gmail.com>
2025-01-16 09:48:45 +0700

    automaticly obtain cookies

--- post-breacher.py
+++ post-breacher.py
@@ -8,33 +8,35 @@ class NusantaraTVScraper:
         self.base_url = "https://nusantaratv.com"
         self.headers = {
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0",
-            "Accept": "*/*",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Accept-Encoding": "gzip, deflate, br, zstd",
-            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "X-Requested-With": "XMLHttpRequest",
-            "Origin": "https://nusantaratv.com",
-            "Referer": "https://nusantaratv.com/",
-            "Connection": "keep-alive",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-origin",
         }
 
         # Set required cookies from curl request
-        self.session.cookies.set(
-            "XSRF-TOKEN",
-            "eyJpdiI6ImZGSjRTSEYyQ3h0WEg3NzRpeGRHbWc9PSIsInZhbHVlIjoiYmUyS09TTmNaU0xIUFVPS0pNRjRaWHV0OUFTQlNzTmJCWjZBQVBhTllHRERwNG1ZL25PRlVSVDVSSG1zTTRpM3dYU1lTYjlFM2pRUWE2c0ZJTDNPeXVWZHhQZmxONkExK1I2Z0h0Zk5TRzIzNzlNV1hUaENkL3NidzNENDg2dzIiLCJtYWMiOiI1MWQ3MjY0MDU5YTZhZWI5Y2Y1MmQwZjA2ZWQ4YzQ2NWU0MWJlMzI1ZWYyYmZiMzE1MGJkY2U3MWQ1MTFmZTEzIiwidGFnIjoiIn0=",
-        )
-        self.session.cookies.set(
-            "nusantaratv_session",
-            "eyJpdiI6IlREdUVPaXd5cnZkRXQxUm9RdXdKTEE9PSIsInZhbHVlIjoiUUF2UXZuZmU2QXVuMEMrdHVnSW90eGFMcWIwVGhjZmlmVVdXcFpRNlE3amFhWXo1SUJiSGIwT3hOcVlDeTRNYVBPWkZHWjZuRnZjUUZMYS9HYlMyQS81ZFM2ZzRsanBETmpQcWg0dmZVaTlOVFRreCtqZWpJSGJURVE3VmFsbmEiLCJtYWMiOiIyYTJjZTY3NTU4MzE0YmZjNDk3YmE2NGM5ZDk4OTgzY2Y4YzczYTU3Y2UxZjRkMGEwNjA2ZjBiODQwYjgwMjA5IiwidGFnIjoiIn0=",
-        )
-        self.session.cookies.set("cProps", "4b5a5e03-0b56-44e3-ad55-cd5681f423e6")
+        # self.session.cookies.get()
+        # self.session.cookies.set("cProps", "4b5a5e03-0b56-44e3-ad55-cd5681f423e6")
 
     def scrape_content(self):
-        csrf_token = "4TCbOcNfg6s18QSdvfduK2cY74sK4W2RFUtrOnkz"
+        response = self.session.get(self.base_url, headers=self.headers)
+        page = BeautifulSoup(response.text, "html.parser")
+        csrf_token = page.find("input", attrs={"name": "_token"})
+        session_cookie = self.session.cookies.get("nusantaratv_session")
+
+        if not csrf_token or not session_cookie:
+            print("Failed to get CSRF token or session cookie")
+            return
+
+        csrf_token = csrf_token["value"]
+
         load_more_url = f"{self.base_url}/loadmore/load_data"
+        self.headers.update(
+            {
+                "X-Requested-With": "XMLHttpRequest",
+                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+            }
+        )
+
+        self.session.cookies.set("XSRF-TOKEN", csrf_token)
+        self.session.cookies.set("nusantaratv_session", session_cookie)
 
         payload = {"id": "", "_token": csrf_token}
 
@@ -43,7 +45,6 @@ class NusantaraTVScraper:
                 load_more_url,
                 data=payload,
                 headers=self.headers,
-                cookies=self.session.cookies,
             )
 
             if response.status_code == 200:
